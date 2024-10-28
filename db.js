32286bf949f034ea4519f22c26bd4824fa0efe70
Patryk Sikorski <sikor87@hotmail.com>
2024-10-28 20:43:21 +0000

    test

--- db.js
+++
@@ -1,7 +0,0 @@
-import pgPromise from "npm:pg-promise";
-const pgp = pgPromise({});
-const db = pgp(
-    "postgres://sikor:TrackerUserPassword@localhost:5432/expense_tracker",
-);
-
-export default db;
